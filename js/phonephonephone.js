774cfc7e3c5a8eefdbe2c9871af9b2b182f99ce1
Arkadiy Kukarkin <arkadiy@hypem.com>
2012-09-12 01:09:35 -0400

    Add onscroll/onresize events, animate scroll

--- js/phonephonephone.js
+++ js/phonephonephone.js
@@ -68,9 +68,11 @@ function run(){
     channel.bind('client-sync_viewport', function(e) {
       console.log(e);
       console.log('trying to scroll y by: '+e.y);
-      $(window).scrollTop(e.y);
-      $(window).scrollLeft(e.x);
+      $(window).animate({scrollTop: e.y});
+      $(window).animate({scrollLeft: e.x});
     });
+    window.scroll(sync_viewport);
+    window.resize(sync_viewport);
 
     Pusher.log('here');
   });
