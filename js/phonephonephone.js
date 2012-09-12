51d8af168769e447238a880696e95a2911c7d0ff
Arkadiy Kukarkin <arkadiy@hypem.com>
2012-09-11 21:50:33 -0400

    Move main code into ppp.js, remove index.php, jslint

--- js/phonephonephone.js
+++ js/phonephonephone.js
@@ -9,10 +9,10 @@ function phonephonephone(activityChannel, options) {
   this._activityChannel = activityChannel;
   
   this._activityChannel.bind('scroll', function(activity) {
-      self._handleActivity.call(self, activity, 'scroll');
-    });
-    
-};
+    self._handleActivity.call(self, activity, 'scroll');
+  });
+
+}
 
 phonephonephone.prototype._handleActivity = function(activity, eventType) {
   var self = this;
@@ -34,3 +34,40 @@ phonephonephone.prototype.sendActivity = function(activityType, activityData) {
   })
 };
 
+
+function init(){
+    // Enable pusher logging - don't include this in production
+    Pusher.log = function(message) {
+      if (window.console && window.console.log) { window.console.log(message) };
+    };
+
+    // Flash fallback logging - don't include this in production
+    WEB_SOCKET_DEBUG = true;
+
+    // set up auth
+    Pusher.channel_auth_endpoint = '/pusher_auth.php';
+
+    // init
+    var pusher = new Pusher('a4e27d5e92ed73bd5abe');
+    var channel = pusher.subscribe('presence-test');
+
+    channel.bind('pusher:subscription_succeeded', function() {
+      console.log('subscribed successfully');
+    });
+
+    $(document).ready(function () {   
+    // need DOM in place for this to make sense
+    var streamer = new phonephonephone(channel);
+    Pusher.log('here');
+  });
+}
+
+
+if (window.jQuery === undefined) {
+  var script = document.createElement('SCRIPT');  
+  script.src = 'http://code.jquery.com/jquery-1.7.1.min.js';   
+  script.onload=init;  
+  document.body.appendChild(script);  
+} else {
+  init();  
+}
