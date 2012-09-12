952e3e171de0cef1bd41c2a9f9c2ba413454c5a5
Arkadiy Kukarkin <arkadiy@hypem.com>
2012-09-12 00:22:42 -0400

    Add header("Access-Control-Allow-Origin: *")

--- pusher_auth.php
+++ pusher_auth.php
@@ -3,7 +3,10 @@ define("APP_KEY", 'a4e27d5e92ed73bd5abe');
 define("APP_SECRET", '82afbc5625bd4770f584');
 define("APP_ID", '24922');
 require_once 'lib/Pusher-PHP/lib/Pusher.php';
+
 $pusher = new Pusher(APP_KEY, APP_SECRET, APP_ID);
 $client_id = bin2hex(openssl_random_pseudo_bytes(64));
 $presence_data = array(); // something about screen position here?
+
+header("Access-Control-Allow-Origin: *");
 echo $pusher->presence_auth($_POST['channel_name'], $_POST['socket_id'], $client_id, $presence_data);
